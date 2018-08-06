7e91d2003f708beae097a203b09026bc52b4e2a4
Hayley Landsberg <hayleyheath@gmail.com>
2018-08-06 14:50:25 -0500

    Employees model and view have been made

---
+++ app/bang/models/employees.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Employees(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    supervisor = models.BooleanField()
+    # department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
