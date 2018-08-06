38c4ef8b70b295b2deea1a7766e9846fe83a77d7
eagobert <eagobert@hotmail.com>
2018-08-06 15:51:08 -0500

    start computerlistview

---
+++ app/bang/views/computer_view.py
@@ -0,0 +1,7 @@
+from django.views.generic import ListView
+from bang.models import 
+#from .forms import AlbumForm
+
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Artist, Album
+from .forms import AlbumForm
