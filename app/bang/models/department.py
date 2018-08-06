5661ab850e5fa68d6d0228101638c966b6918dd1
rafcevallos <rafael.cevallos81@gmail.com>
2018-08-06 14:36:26 -0500

    Added Meta class to Department model

--- app/bang/models/department.py
+++ app/bang/models/department.py
@@ -5,5 +5,8 @@ from django.db import models
 class Department(models.Model):
     dept_name = models.CharField(default="", max_length=30)
 
+    class Meta:
+        db_table = "department"
+
     def __str__(self):
         return self.dept_name
