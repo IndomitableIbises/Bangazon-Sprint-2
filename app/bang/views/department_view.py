1651c8e951fbeab25414a11c1565591df1dc4f49
rafcevallos <rafael.cevallos81@gmail.com>
2018-08-06 15:13:44 -0500

    Department Form and views created

--- app/bang/views/department_view.py
+++ app/bang/views/department_view.py
@@ -2,7 +2,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, FormView, DetailView, CreateView
 from bang.models import Department
-# from .forms import -- Forms will be imported here
+from bang.forms import DepartmentForm
 
 
 class DepartmentListView(ListView):
@@ -17,5 +17,22 @@ class DepartmentListView(ListView):
         context['location'] = 'departments'
         return context
 
-# class DepartmentFormView(FormView):
-#     template_name = 'bang/department_form.html'
+class DepartmentFormView(FormView):
+    """
+    API endpoint that allows a DEPARTMENT FORM to be viewed and edited.
+    """
+    template_name = 'bang/department_form.html'
+    form_class = DepartmentForm
+    success_url = '/bang/departments/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['location'] = 'departments'
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super(DepartmentFormView, self).form_valid(form)
+
+class DepartmentDetailView(DetailView):
+    model = Department
