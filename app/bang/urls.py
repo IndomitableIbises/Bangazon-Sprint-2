c5c724c24aad2364ecd2b8d03af7988f18b50218
rafcevallos <rafael.cevallos81@gmail.com>
2018-08-06 15:37:24 -0500

    Trying to fix FieldError

--- app/bang/urls.py
+++ app/bang/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 app_name = 'bang'
 urlpatterns = [
+    path('', views.IndexView.as_view(), name = 'index'),
 
+##########
+# DEPARTMENT PATHS by Raf
+    path('departments/', views.DepartmentListView.as_view(), name='departments'),
+    path('departments/add/', views.DepartmentFormView.as_view(), name='department_form'),
+    path('departments/<int:pk:>/', views.DepartmentDetail.as_view(), name='department_detail'),
 ]
--- app/bang/views/__init__.py
+++ app/bang/views/__init__.py
@@ -1,1 +1,2 @@
+from .index_view import IndexView
 from .department_view import DepartmentListView
